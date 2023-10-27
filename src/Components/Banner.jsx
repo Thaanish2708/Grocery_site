40ccdfe2cd1c77a4a576b8c7ad9d4daf616e47c7
Sharan Ranganathan <sharanr@crayondata.com>
2023-10-27 14:15:15 +0530

    withAPIcall

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,10 +3,8 @@
 function Banner(){
 
     return (
-        <div className='row p-0' style={{width:"100%"}}>
-            <div style={{padding:"0px",paddingTop:"15px"}}>
-                <img src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=2700/layout-engine/2022-05/Group-33704.jpg" alt="banner" style={{width:"80%"}}/>
-            </div>
+        <div className='row pt' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=2700/layout-engine/2022-05/Group-33704.jpg" alt="banner" style={{ height:"17vw",width: "100vw", maxWidth: "100%", paddingTop: "10px" }}/>
       </div>
     )
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,28 @@
 import './LoginModal.css'; // Create a new CSS file for your custom styles
 
 import LoginModal from './LoginModal';
+import './LoginModal.css'; // Create a new CSS file for your custom styles
+
 
 function Header() {
   
 
   return (
-    <div class="container-fluid m-0 border-bottom">
+    
       <div class="row m-0 ">
-        <div className='col-md-1'>
-          <img src="./logo.png" alt="logo" height="100px"/>
+        <div className='col-md-1' style={{height:"15%",marginRight:"15px"}}>
+          <img src="./logo.png" alt="logo" height="100vw" />
         </div>
         
 
-        <form class="col-md-9 mt-auto mb-auto ml-auto" role="search">
+        <form class="col-md-8 mt-auto mb-auto" role="search">
           <input type="search" class="form-control text" placeholder="Search..." aria-label="Search" size="100" />
         </form>
+        
 
-        <div  className="col-md-1 mt-auto mb-auto">
-          <button type="button" class="btn btn-secondary" style={{width:"120px", height:"40px"}}>My Cart</button>
+        <div  className="col-md-1 mt-auto mb-auto" style={{marginLeft:"50px"}}>
+        <img src='download.png' height="25vw" width="25vw" /><span>
+          <button type="button" class="btn" style={{width:"8vw"}}>My Cart</button> </span>
         </div> 
 
         <div  className="col-md-1 mt-auto mb-auto" >
@@ -26,7 +30,6 @@ function Header() {
         </div>
 
       </div>
-    </div>
         
   );
 }
