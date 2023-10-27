abe18ff489885dad4bad665b162d51c59b9b78fa
Nidin S <nidins@crayondata.com>
2023-10-27 16:03:21 +0530

    First API

--- src/App.js
+++ src/App.js
@@ -10,23 +10,43 @@ import './Components/Card.css';
 import React, { useState, useRef } from 'react'; // Import useState and useRef
  
 function App() {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 6,
-  };
- 
-  const cards = Array.from({ length: 10 }, (_, index) => <Card key={index} />);
- 
-  const [currentSlide, setCurrentSlide] = useState(0); 
-  const sliderRef = useRef(null); 
- 
-  const nextSlide = () => {
-    // Increase the current slide index by the number of slides to scroll
-    setCurrentSlide(currentSlide + settings.slidesToScroll);
-    // Use the Slider ref to move to the next slide
-    sliderRef.current.slickNext();
+  const [productData, setProductData] = useState({});
+  const categories = [1, 2]; // Add all your categories here
+
+  useEffect(() => {
+    // Fetch data for each category and store it in productData
+    const fetchData = async () => {
+      const data = {};
+      for (const category of categories) {
+        const productList = await getProductlist(category);
+        data[category] = productList;
+      }
+      setProductData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  const getProductlist = async (category) => {
+    try {
+      
+      const response = await fetch(`http://localhost:8080/category/${category}`, {
+        method: "GET",
+      });
+
+      if (response.status === 200) {
+          const data = await response.json();
+          console.log("Products", data);
+          return data;
+        // Reset the form or perform any other actions as needed
+      } else {
+          console.log(response);
+          console.log(response.status);
+        console.error("Failed to add employee.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
  
   return (
